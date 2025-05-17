2c4a25397cc3660b64d9873adf30f123774d6a17
ibtissam <ibtissammahdi315@gmail.com>
2025-05-17 02:02:20 +0100

    Ton message de commit ici

--- src/main/java/com/example/projetjava/view/SimpleView.java
+++ src/main/java/com/example/projetjava/view/SimpleView.java
@@ -11,7 +11,7 @@ public class SimpleView extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/simpleview.fxml"));
-        Scene scene = new Scene(root, 400, 400);
+        Scene scene = new Scene(root, 600, 600);
         stage.setScene(scene);
         stage.setTitle("Chatbot Simple");
         stage.show();
